01f76bd11a74883c3b557fb12ad43e6126f7d2dd
Wisniewski <wisniewk@emea.roche.com>
2023-03-09 16:57:22 +0100

    Update project java-springboot-helloworld-azurestorageaccount-upload-download

--- azure-storage-account/java-springboot-helloworld-azurestorageaccount-upload-download/src/main/java/com/example/controllers/HelloWorldController.java
+++ azure-storage-account/java-springboot-helloworld-azurestorageaccount-upload-download/src/main/java/com/example/controllers/HelloWorldController.java
@@ -1,39 +1,41 @@
 package com.example.controllers;
 
+import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.jsons.HelloWorldJson;
+import com.example.services.AzureStorageAccountService;
 
 @RestController
 public class HelloWorldController {
 	
 	Logger logger = LoggerFactory.getLogger(HelloWorldController.class);
 	
-	private Environment environment;
-	
-	@Value("${service.helloworld.message}")
-	private String message;
-	
+	private AzureStorageAccountService azureStorageAccountService;
+
 	@Autowired
-	public HelloWorldController(Environment environment) {
-		this.environment = environment;
+	public HelloWorldController(AzureStorageAccountService azureStorageAccountService) {
+		this.azureStorageAccountService = azureStorageAccountService;
 	}
+
+
 
 	@RequestMapping(value="/")
 	public HelloWorldJson helloWorld() {
-				
-		String port = environment.getProperty("local.server.port");
-		String uuid = System.getProperty("uuid");
 		
-		logger.info("Application was called with message: {}, port: {} and uuid: {}", message, port, uuid);
+		List<String> listFiles = azureStorageAccountService.listFiles();
+		for (String file : listFiles) {
+			System.out.println(file);
+		}
 		
-		return new HelloWorldJson(message, port, uuid);
+		logger.info("Application was called with message: {}", "null");
+		
+		return new HelloWorldJson(null);
 		
 	}
 	
--- azure-storage-account/java-springboot-helloworld-azurestorageaccount-upload-download/src/main/java/com/example/jsons/HelloWorldJson.java
+++ azure-storage-account/java-springboot-helloworld-azurestorageaccount-upload-download/src/main/java/com/example/jsons/HelloWorldJson.java
@@ -3,15 +3,11 @@ package com.example.jsons;
 public class HelloWorldJson {
 	
 	private String message;
-	private String port;
-	private String uuid;
 	
 	public HelloWorldJson() {}
 	
-	public HelloWorldJson(String message, String port, String uuid) {
+	public HelloWorldJson(String message) {
 		this.message = message;
-		this.port = port;
-		this.uuid = uuid;
 	}
 
 	public String getMessage() {
@@ -20,19 +16,5 @@ public class HelloWorldJson {
 	public void setMessage(String message) {
 		this.message = message;
 	}
-
-	public String getPort() {
-		return port;
-	}
-	public void setPort(String port) {
-		this.port = port;
-	}
-
-	public String getUuid() {
-		return uuid;
-	}
-	public void setUuid(String uuid) {
-		this.uuid = uuid;
-	}	
 	
 }
